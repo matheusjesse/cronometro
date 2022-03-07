ee1194f81dc9868a410908364d73811c3e404560
matheusjesse <matheus_jesse@hotmail.com>
2022-03-06 23:28:41 -0300

    implementando o botao restart

--- src/App.js
+++ src/App.js
@@ -11,22 +11,28 @@ class App extends React.Component {
 
     this.handleClick = this.handleClick.bind(this);
     this.handleCronometro = this.handleCronometro.bind(this);
+    this.handleRestart= this.handleRestart.bind(this);
 
     this.state = {
       cronometroMin: '00',
-      cronometroSec: '30',
+      cronometroSec: '00',
       presetMinUm: '1',
       presetMinTres: '3',
       presetMinSeis: '6',
       presetSec: '30',
       buttonDisabled: false,
+      cronometroOn: true,
     };
   }
 
   handleClick({ target }) {
     //const { name, value } = target;
-    const { cronometroMin, } = this.state;
-    const acc = parseFloat(cronometroMin) + parseFloat(target.value);       
+    const { cronometroMin, cronometroOn } = this.state;
+    const acc = parseFloat(cronometroMin) + parseFloat(target.value);
+    
+    if(cronometroOn === false) {
+      this.setState({ cronometroOn: true});   
+    }
     if(acc < 10) {
       this.setState({cronometroMin: `0${acc}`});
     }else{
@@ -35,17 +41,17 @@ class App extends React.Component {
   }
 
   handleCronometro() {
-    const { cronometroMin, cronometroSec, buttonDisabled } = this.state;
+    const { cronometroMin, cronometroSec, buttonDisabled, cronometroOn } = this.state;
 
     const sec = parseFloat(cronometroSec);
     const min = parseFloat(cronometroMin);
-    
-    if(buttonDisabled === false) {
+    const validCronometro = sec + min;
+
+    if(buttonDisabled === false && validCronometro > 0) {
       this.setState({ buttonDisabled: true });
     }
-
     if (sec > 0) {
-      this.setState( { cronometroSec: sec -1 } );
+      this.setState( { cronometroSec: sec -1 } );      
       if(sec < 10) {
         this.setState({ cronometroSec:`0${sec -1}`})
       }
@@ -55,8 +61,23 @@ class App extends React.Component {
         this.setState({ cronometroMin:`0${min -1}`})
       }
     }
+    
+    if(cronometroOn === true){
+      setTimeout(this.handleCronometro, 1000);    
+    }
+  }
 
-    setTimeout(this.handleCronometro, 1000);    
+  handleRestart() {
+    const { buttonDisabled, } = this.state;
+    
+    if( buttonDisabled === true) {
+      this.setState( {
+        buttonDisabled: false, 
+        cronometroMin: '00',
+        cronometroSec: '00',
+        presetSec: '00',
+        cronometroOn: false});
+    }
   }
 
   render() {
@@ -79,6 +100,7 @@ class App extends React.Component {
           buttonDisabled={ buttonDisabled }
         />
         <Restart
+          handleRestart={ this.handleRestart }
         />
       </>
     );
--- src/components/Restart.js
+++ src/components/Restart.js
@@ -2,8 +2,9 @@ import React from "react";
 
 class Restart extends React.Component {
   render() {
+    const { handleRestart } = this.props;
     return(
-      <input type="button" name="restart" value="Reiniciar"/>
+      <input type="button" name="restart" value="Reiniciar" onClick={ handleRestart }/>
     );
   }
 }
